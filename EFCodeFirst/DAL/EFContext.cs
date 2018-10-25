7cf980154247a403f977b884607a842fefdf3485
Eduardo <eduardosilva-9@hotmail.com>
2018-10-25 08:28:17 -0300

    Meu primeiro commit da universidade codefirst em entityframwork

---
+++ EFCodeFirst/DAL/EFContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirst.DAL
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("EFConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Universidade> Universidades { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Curso> Cursos  { get; set; }
+    }
+}
